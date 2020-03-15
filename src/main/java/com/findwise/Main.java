092f66cab36548420fb91c02b679ccb35bdd9d18
Mateusz Mizio <mateusz1997-11@o2.pl>
2020-03-15 15:23:04 +0100

    run menu without functionality

--- src/main/java/com/findwise/Main.java
+++ src/main/java/com/findwise/Main.java
@@ -1,18 +1,17 @@
 package com.findwise;
 
+import com.findwise.ui.MenuEngine;
+
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
+
+    private static MenuEngine menuEngine = new MenuEngine();
+
     public static void main(String[] args) throws IOException {
 
-        while (true){
-
-            Scanner scanner = new Scanner(System.in);
-            printMainMenu();
-            String decisionMainManu = scanner.nextLine();
-
-
+        while (true) {
+            menuEngine.mainMenu();
         }
 
 
@@ -25,30 +24,6 @@ public class Main {
 //        List<IndexEntry> indexEntries = fileLoader.getSearchEngine().search(term);
 //        System.out.println(indexEntries);
     }
-
-    private static void printMainMenu() throws IOException {
-        System.out.println("==========================================");
-        System.out.println("The Simple Search Engine");
-        System.out.println("1. Add a document.");
-        System.out.println("2. Analyze a document.");
-        System.out.println("3. Exit");
-        System.out.println("Type your decision: ");
-    }
-
-    private static void printAddMenu() throws IOException{
-        System.out.println("==========================================");
-        System.out.println("The Simple Search Engine");
-        System.out.println("1. Back to Main Menu");
-        System.out.println("Enter document path: ");
-    }
-
-    private static void printAnalyzeMenu(){
-        System.out.println("==========================================");
-        System.out.println("The Simple Search Engine");
-        System.out.println("1. Back to Main Menu");
-        System.out.println("1. Back to Main Menu");
-    }
-
 }
 
 
